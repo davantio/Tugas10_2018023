100bcc6e643bf07f5e7287c46ea135f8a8d06f56
davantio <davantiodestama07@gmail.com>
2022-11-26 21:37:11 +0700

    Work Manager

--- app/src/main/java/com/example/surabayavirtualtourism/InfoPecel.java
+++ app/src/main/java/com/example/surabayavirtualtourism/InfoPecel.java
@@ -3,13 +3,19 @@ package com.example.surabayavirtualtourism;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.surabayavirtualtourism.databinding.ActivityInfoPecelBinding;
+
 public class InfoPecel extends AppCompatActivity {
+    private ActivityInfoPecelBinding binding;
     RecyclerView recyclerView;
 
     String s1[], s2[];
@@ -19,7 +25,18 @@ public class InfoPecel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info_pecel);
+        binding = ActivityInfoPecelBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        final OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+
+        binding.btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WorkManager.getInstance().enqueueUniqueWork(
+                        "Notifikasi", ExistingWorkPolicy.REPLACE, request);
+            }
+        });
 
         recyclerView = findViewById(R.id.rv_listkuliner);
 
